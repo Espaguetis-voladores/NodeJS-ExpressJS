a66eb0408bf361f2a03cc91ae0e68bf6db64e82a
Polbasetis <pol.mendez@basetis.com>
2022-10-14 13:07:10 +0200

    Express handlebars instaled

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("HOla")
+const express = require('express')
+const Handlebars = require("handlebars");
+const app = express()
+const port = 8080
+
+
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils" }));
+
+app.get('/', (req, res) => {
+  res.send( {mensaje : 'Hello World!'} ) 
+})
+
+app.listen(port, () => {
+  console.log(`La app esta escuchando al puerto ${port}`)
+})
